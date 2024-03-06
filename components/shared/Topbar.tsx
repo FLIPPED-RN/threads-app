0d3e2855cef1ac76da646743699649f8d1498b07
FLIPPED-RN <rnazarov142@gmail.com>
2024-03-07 01:18:55 +0200

    v24.0.7 (dev)

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,4 @@
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -37,6 +37,15 @@ function Topbar() {
             </SignOutButton>
           </SignedIn>
         </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4"
+            }
+          }}
+        />
+
       </div>
     </nav>
   )
