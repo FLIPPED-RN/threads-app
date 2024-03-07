1f1e3b38eaac48901cc2c09fc2aecab713a1d913
FLIPPED-RN <rnazarov142@gmail.com>
2024-03-07 22:37:14 +0200

    v24.0.18 (dev version)

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,8 @@ import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { z } from "zod";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from '@/lib/uploadthing'
 
 interface Props {
   user: {
@@ -72,6 +74,10 @@ const AccountProfile = ({user, btnTitle}: Props) => {
     const blob = values.profile_photo;
 
     const hasImageChanged = isBase64Image(blob);
+
+    if(hasImageChanged) {
+      const imgRes = 
+    }
   }
 
   return(
