f06d4641aa5000e6166c35ae77af51e68fd2c5f1
FLIPPED-RN <rnazarov142@gmail.com>
2024-03-07 22:40:14 +0200

    v24.0.19 (dev version)

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -37,6 +37,7 @@ interface Props {
 const AccountProfile = ({user, btnTitle}: Props) => {
 
   const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
   
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -70,13 +71,15 @@ const AccountProfile = ({user, btnTitle}: Props) => {
     }
   }
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
     const blob = values.profile_photo;
 
     const hasImageChanged = isBase64Image(blob);
 
     if(hasImageChanged) {
-      const imgRes = 
+      const imgRes = await startUpload(files);
+
+      
     }
   }
 
