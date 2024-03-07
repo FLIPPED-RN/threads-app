564e1965fff7b7db52f3d103ce9c8e265bd38924
FLIPPED-RN <rnazarov142@gmail.com>
2024-03-07 22:22:49 +0200

    v24.0.16 (dev version)

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -46,14 +46,32 @@ const AccountProfile = ({user, btnTitle}: Props) => {
     }
   });
 
-  const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
+  const handleImage = (e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void) => {
     e.preventDefault();
+
+    const fileReader = new FileReader();
+
+    if(e.target.files && e.target.files.length > 0){
+      const file = e.target.files[0];
+
+      setFiles(Array.from(e.target.files));
+
+      if(!file.type.includes('image')) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || '';
+
+        fieldChange(imageDataUrl);
+      }
+
+      fileReader.readAsDataURL(file);
+    }
   }
 
   function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    const blob = values.profile_photo;
+
+    const hasImageChanged = isBase64Image(blob);
   }
 
   return(
